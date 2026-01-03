33a3ddd7651bbf3b696ff3ac208453a2d3c42810
David Carrera <mail@davidcarreracasado.cat>
2026-01-03 19:04:04 +0100

    add frame profiling facilities

--- gravity/application.py
+++ gravity/application.py
@@ -1,6 +1,10 @@
 import logging
+import time
+from collections import deque
+from contextlib import contextmanager
 
 import pygame
+from typing_extensions import Optional
 
 from gravity.camera import Camera, CameraController
 from gravity.config import AppConfig
@@ -13,6 +17,49 @@ from gravity.physics import (
 )
 from gravity.render import Renderer
 from gravity.ui import CursorUIController, InspectorUIState, PauseController
+
+logger = logging.getLogger(__name__)
+
+
+class _Timer:
+    def __init__(self, count: int) -> None:
+        self._target_count = count
+        self._current_count = 0
+        self._samples: dict[str, deque[float]] = {}
+
+    def count(self) -> None:
+        self._current_count += 1
+
+    @contextmanager
+    def time(self, name: str):
+        t0 = time.perf_counter()
+        try:
+            yield
+        finally:
+            t1 = time.perf_counter()
+            self.sample(name, t1 - t0)
+
+    def sample(self, name: str, sample: float) -> None:
+        samples = self._samples.setdefault(name, deque())
+        samples.append(sample)
+
+        if len(samples) > self._target_count:
+            samples.popleft()
+
+    def average(self, name: str) -> float:
+        samples = self._samples.get(name)
+        if samples is None:
+            return 0.0
+        res = sum(samples) / len(samples)
+        return res
+
+    def reset(self) -> None:
+        self._current_count = 0
+        for samples in self._samples.values():
+            samples.clear()
+
+    def done(self) -> bool:
+        return self._current_count >= self._target_count
 
 
 class Application:
@@ -30,8 +77,10 @@ class Application:
         self._events = events
         self._renderer = renderer
 
+        self._timer = _Timer(100)
+
     def _initialize(self) -> None:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
 
         pygame.init()
         pygame.font.init()
@@ -61,14 +110,36 @@ class Application:
 
         clock = pygame.time.Clock()
         while self._game.is_running():
-            dt = clock.get_time() / 1000.0
-            self._events.handle_events()
-            self._game.update(dt)
-            self._renderer.render(self._game)
-            clock.tick(self._target_framerate)
+            dt = clock.tick(self._target_framerate) / 1000.0
+            self._timer.sample("dt", dt)
+
+            with self._timer.time("events"):
+                self._events.handle_events()
+            with self._timer.time("update"):
+                self._game.update(dt)
+            with self._timer.time("render"):
+                self._renderer.render(self._game)
+
+            self._timer.count()
+            self._show_frame_stats()
 
         self._deinitialize()
         return 0
+
+    def _show_frame_stats(self):
+        if not self._timer.done():
+            return
+
+        dt = self._timer.average("dt")
+        fps = 1 / dt
+        logger.debug(f"fps: {fps:.3}")
+
+        for name in ("events", "update", "render"):
+            value = self._timer.average(name)
+            percent = value / dt * 100
+            logger.debug(f"{name}: {percent:.3}%")
+
+        self._timer.reset()
 
 
 def build_application(config: AppConfig) -> Application:
